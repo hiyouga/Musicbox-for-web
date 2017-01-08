c27433d8d18385058029aaf4ca08d87a533c27e4
hoshi hiyouga <admin@hiyouga.tk>
2017-01-08 13:16:05 +0800

    Add files via upload

---
+++ player.js
@@ -0,0 +1,175 @@
+﻿oAudio = document.getElementById('player');
+play = $("#play");
+album = $("#album");
+inn = $("#in");
+music_name = $("#music_name");
+artist = $("#artist");
+neurl = $("#neurl");
+cd = $("#cd");
+lrc_row = $("#lrc");
+bar = $("#bar");
+$(document).ready(function () {
+	cd_size();
+	$.getScript("list.js", function() {
+		$.each(musiclist,function(index,item){
+			$("#musiclist").append('<a href="javascript:load_music('+item.id+');"><li><strong>'+item.title+'</strong>&nbsp;&nbsp;--&nbsp;&nbsp;<small>'+item.singer+'&nbsp;&nbsp;(♥'+item.like+')</small></li></a>');
+		});
+		var n = Math.floor(Math.random() * musiclist.length);
+		id = musiclist[n].id;
+		$.get("player.php?type=play&id=" + id, function (data) {
+			mp3_info = JSON.parse(data);
+			$("#player").attr("src", mp3_info.mp3);
+			$("#like_music").attr("href", "javascript:like_music("+id+");");
+			$("#dl_music").attr("href", mp3_info.mp3);
+			album.css("background-image", "url('" + mp3_info.cover + "?param=100y100')");
+			music_name.html(mp3_info.music_name);
+			artist.html(mp3_info.artists);
+			neurl.attr("href", 'http://music.163.com/song?id='+id);
+			if (mp3_info.lrc != "no") {
+				lrc = mp3_info.lrc;
+			} else {
+				lrc = "no";
+			}
+		});
+	});
+	oAudio.volume = 0.5;
+});
+$(window).resize(function () {
+    cd_size();
+});
+$("#player").bind("ended", function () {
+    if (lrc != "no") {
+        clearInterval(lrc_interval);
+    }
+    next_music();
+});
+function m_play() {
+    if (oAudio.paused) {
+        oAudio.play();
+		play.removeClass("ico_play");
+		play.addClass("ico_pause");
+		$("#miniplay").removeClass("ico_mini_play");
+		$("#miniplay").addClass("ico_mini_pause");
+        if (lrc != "no") {
+            lrc_interval = setInterval("display_lrc()", 1000);
+        }
+    }
+    else {
+        oAudio.pause();
+		play.removeClass("ico_pause");
+		play.addClass("ico_play");
+		$("#miniplay").addClass("ico_mini_play");
+		$("#miniplay").removeClass("ico_mini_pause");
+        if (lrc != "no") {
+            clearInterval(lrc_interval);
+        }
+    }
+}
+function next_music() {
+    oAudio.pause();
+    if (!oAudio.paused && lrc != "no") {
+        clearInterval(lrc_interval);
+    }
+	var n = Math.floor(Math.random() * musiclist.length);
+	id = musiclist[n].id;
+    load_music(id);
+}
+function load_music(id) {
+    $.get("player.php?type=play&id=" + id, function (data) {
+        mp3_info = JSON.parse(data);
+        $("#player").attr("src", mp3_info.mp3);
+		$("#like_music").attr("href", "javascript:like_music("+id+");");
+		$("#dl_music").attr("href", mp3_info.mp3);
+        album.css("background-image", "url('" + mp3_info.cover + "?param=100y100')");
+        music_name.html(mp3_info.music_name);
+        artist.html(mp3_info.artists);
+		play.addClass("ico_pause");
+		play.removeClass("ico_play");
+		$("#like_music").removeClass("ico_unlike");
+		$("#like_music").addClass("ico_like");
+		$("#miniplay").removeClass("ico_mini_play");
+		$("#miniplay").addClass("ico_mini_pause");
+		neurl.attr("href", 'http://music.163.com/song?id='+id);
+        oAudio.play();
+        lrc_row.html("");
+        if (mp3_info.lrc != "no") {
+            lrc = mp3_info.lrc;
+            lrc_interval = setInterval("display_lrc()", 1000);
+        } else {
+            lrc = "no";
+        }
+    });
+}
+function volume(vol) {
+    oAudio.volume = vol / 10;
+}
+function m_mute() {
+	if(oAudio.volume != 0){
+		$("#volume").addClass("ico_mute");
+		oAudio.volume = 0;
+	}else{
+		$("#volume").removeClass("ico_mute");
+		oAudio.volume = 0.5;
+	}
+	
+}
+function cd_size() {
+    cd_height = cd.height();
+    cd.css("width", cd_height);
+}
+function display_lrc() {
+    play_time = Math.floor(oAudio.currentTime).toString();
+    lrc_row.html(lrc[play_time]);
+}
+oAudio.addEventListener('timeupdate',function(){
+	if (!isNaN(oAudio.duration)) {
+		var progressValue = (oAudio.currentTime/oAudio.duration)*100;
+		if(progressValue == 1){
+			progressValue = 0;
+		}
+		bar.css("width",progressValue+'%');
+	};
+},false);
+function toggle(){
+	$("#lrcwarp").fadeOut(100);
+	$("#listwarp").fadeOut(100);
+	$("#panel").delay(100).animate({width:'hide'},500);
+	$("#mini_panel").delay(620).animate({width:'toggle'},200);
+	$("#hiyougaplayer").css({
+		"height":"50px",
+		"width":"65px"
+	});
+}
+function mini_toggle(){
+	$("#mini_panel").animate({width:'hide'},200);
+	$("#panel").delay(220).animate({width:'toggle'},500);
+	$("#hiyougaplayer").css({
+		"height":"380px",
+		"width":"850px"
+	});
+}
+function showlrc(){
+	$("#lrcwarp").fadeToggle(300);
+}
+function showlist(){
+	$("#listwarp").fadeToggle(300);
+}
+function close_panel(){
+	oAudio.pause();
+	$("#hiyougaplayer").fadeOut();
+}
+function add_music(){
+	newid = $("#add_music").val();
+	$.get("player.php?type=upload&id=" + newid, function (data) {
+		alert(data);
+    });
+	$("#add_music").val("");
+}
+function like_music(id){
+	$.get("player.php?type=dolike&id=" + id, function (data) {
+		if (data = "success") {
+			$("#like_music").removeClass("ico_like");
+			$("#like_music").addClass("ico_unlike");
+		}
+    });
+}
---
+++ player.php
@@ -0,0 +1,133 @@
+﻿<?php
+if($_GET['type']=='play'){
+	//获取数据
+	$id = $_GET['id'];
+	$music_info = json_decode(get_music_info($id), true);
+	$lrc_info = json_decode(get_music_lyric($id), true);
+	//处理音乐信息
+	$play_info["cover"] = $music_info["songs"][0]["album"]["picUrl"];
+	$play_info["mp3"] = $music_info["songs"][0]["mp3Url"];
+	$play_info["mp3"] = str_replace("http://m", "http://p", $play_info["mp3"]);
+	$play_info["music_name"] = $music_info["songs"][0]["name"];
+	foreach ($music_info["songs"][0]["artists"] as $key) {
+		if (!isset($play_info["artists"])) {
+			$play_info["artists"] = $key["name"];
+		} else {
+			$play_info["artists"] .= "," . $key["name"];
+		}
+	}
+	//处理歌词
+	if (isset($lrc_info["lrc"]["lyric"])) {
+		$lrc = explode("\n", $lrc_info["lrc"]["lyric"]);
+		array_pop($lrc);
+		foreach ($lrc as $rows) {
+			$row = explode("]", $rows);
+			if (count($row) == 1) {
+				$play_info["lrc"] = "no";
+				break;
+			} else {
+				$lyric = array();
+				$col_text = end($row);
+				array_pop($row);
+				foreach ($row as $key) {
+					$time = explode(":", substr($key, 1));
+					$time = $time[0] * 60 + $time[1];
+					$play_info["lrc"][$time] = $col_text;
+				}
+			}
+		}
+	} else {
+		$play_info["lrc"] = "no";
+	}
+	echo json_encode($play_info);
+}
+if($_GET['type']=='upload'){
+	$id = $_GET['id'];
+	$music_info = json_decode(get_music_info($id), true);
+	$title = $music_info["songs"][0]["name"];
+	foreach ($music_info["songs"][0]["artists"] as $key) {
+		if (!isset($play_info["artists"])) {
+			$artists = $key["name"];
+		} else {
+			$artists .= "," . $key["name"];
+		}
+	}
+	//检查错误
+	if(empty($title)){
+		echo '网易云音乐链接错误，请重新提交！';
+		exit;
+	}
+	$play_info["mp3"] = $music_info["songs"][0]["mp3Url"];
+	$mp3 = str_replace("http://m", "http://p", $play_info["mp3"]);
+	if(get_headers($mp3)[4]=='Content-Length: 168'){
+		echo '该歌曲不属于支持范围内，请重新选曲！';
+		exit;
+	}
+	$contents = file_get_contents('list.js');
+	if(!!strpos($contents,$title)){
+		echo '歌曲已存在，请不要重复提交！';
+		exit;
+	}
+	$contents = str_replace('var musiclist = [','',$contents);
+	$contents = str_replace('];','',$contents);
+	preg_match_all("#\{(.+?)\}#is",$contents,$arr);
+	$newarr = array(); 
+	foreach ($arr[0] as $k=>$v) {
+		$newarr[$k] = json_decode($v,true);
+	}
+	$add = array("title"=>$title,"singer"=>$artists,"like"=>"0","id"=>$id);
+	$newarr[] = $add;
+	foreach ($newarr as $k=>$v) {
+		$recontents .= json_encode($v).',';
+	}
+	$recontents = 'var musiclist = ['.$recontents.'];';
+	$num = file_put_contents('list.js',$recontents);
+	echo '提交成功';
+}
+if($_GET['type']=='dolike'){
+	$id = $_GET['id'];
+	$contents = file_get_contents('list.js');
+	$contents = str_replace('var musiclist = [','',$contents);
+	$contents = str_replace('];','',$contents);
+	preg_match_all("#\{(.+?)\}#is",$contents,$arr);
+	$newarr = array();
+	foreach ($arr[0] as $k=>$v) {
+		$p = json_decode($v,true);
+		if(in_array($id,$p)){
+			$p['like'] = $p['like']+1;
+		}
+		$newarr[$k] = $p;
+	}
+	foreach ($newarr as $k=>$v) {
+		$recontents .= json_encode($v).',';
+	}
+	$recontents = 'var musiclist = ['.$recontents.'];';
+	$num = file_put_contents('list.js',$recontents);
+	echo 'success';
+}
+if($_GET['type']=='delect'){
+	
+}
+function curl_get($url)
+{
+    $header[] = "Cookie: " . "appver=1.5.0.75771;";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+    curl_setopt($ch, CURLOPT_REFERER, "http://music.163.com/");
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+function get_music_info($music_id)
+{
+    $url = "http://music.163.com/api/song/detail/?id=" . $music_id . "&ids=%5B" . $music_id . "%5D";
+    return curl_get($url);
+}
+function get_music_lyric($music_id)
+{
+    $url = "http://music.163.com/api/song/lyric?os=pc&id=" . $music_id . "&lv=-1&kv=-1&tv=-1";
+    return curl_get($url);
+}
