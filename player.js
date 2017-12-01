b4ffddec9c84a6d2e60284b0857227e5758b36bf
hoshi-hiyouga <467089858@qq.com>
2017-12-01 14:21:22 +0800

    添加"上一曲"功能

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-var txt = '<div class="mini_panel" id="mini_panel"><a href="javascript:m_play();" title="播放/暂停"><i class="ico_ctrl ico_mini_play" id="miniplay"></i><span class="mask"></span><img class="pic" src="default_cover.jpg"/></a><div class="mini_unfold"><a href="javascript:mini_toggle();" class="ico_ctrl ico_unfold" title="展开"></a></div><a href="javascript:close_panel();" class="ico_ctrl ico_close mini_close" title="关闭"></a></div><div class="panel" id="panel"><div class="s-progress"><div id="bar" class="s-bar" style="width:0%;"></div></div><div id="album" class="album"></div><div class="s-info"><div><span id="music_name"></span><br /><span id="artist"></span><br /><span class="neurl">来自：<a id="neurl" rel="nofollow" target="_black" href="">网易云</a></span></div></div><div class="s-action"><div><a href="javascript:prev_music();" id="pre" class="ico_ctrl ico_prev" title="上一首"></a><a style="float:;padding: ;height:;" href="javascript:m_play();" id="play" class="ico_ctrl ico_play" title="播放/暂停"></a><a href="javascript:next_music();" id="next" class="ico_ctrl ico_next" title="下一首"></a><a href="javascript:m_mute();" id="volume" class="ico_ctrl ico_vol" title="音量"></a><a><input id="range" type="range" min="0" max="10" value="5" onchange="volume(this.value);"></a><a href="javascript:like_music();" id="like_music" class="ico_ctrl ico_like" title="赞"></a><a download href="" id="dl_music" class="ico_ctrl ico_share" title="下载" onClick="showtip();"></a><a href="javascript:showlist();" class="ico_ctrl ico_list" title="展开列表"></a><a href="javascript:showlrc();" class="ico_lyric ico_lyric_open" title="显示歌词">词</a></div></div><div class="s-toggle"><a href="javascript:toggle();" class="ico_ctrl ico_fold" title="折叠"></a></div></div><div id="lrcwarp" class="lrcwarp"><span id="lrc" class="s-lrc"></span></div><div id="listwarp" class="listwarp"><div class="upload"><div class="titlebox"><span class="ptitle">播放列表</span><span class="ani_border"></span></div><div class="addbox"><input id="add_music" type="text" placeholder="网易云id" /><a href="javascript:add_music();">+</a></div></div><div id="musiclist" class="musiclist"></div></div><audio id="player"></audio>';
+var txt = '<div class="mini_panel" id="mini_panel"><a href="javascript:m_play();" title="播放/暂停"><i class="ico_ctrl ico_mini_play" id="miniplay"></i><span class="mask"></span><img class="pic" src="default_cover.jpg"/></a><div class="mini_unfold"><a href="javascript:mini_toggle();" class="ico_ctrl ico_unfold" title="展开"></a></div><a href="javascript:close_panel();" class="ico_ctrl ico_close mini_close" title="关闭"></a></div><div class="panel" id="panel"><div class="s-progress"><div id="bar" class="s-bar" style="width:0%;"></div></div><div id="album" class="album"></div><div class="s-info"><div><span id="music_name"></span><br /><span id="artist"></span><br /><span class="neurl">来自：<a id="neurl" rel="nofollow" target="_black" href="">网易云</a></span></div></div><div class="s-action"><div><a href="javascript:prev_music();" nowdata="" id="pre" class="ico_ctrl ico_prev" title="上一首"></a><a style="float:;padding: ;height:;" href="javascript:m_play();" id="play" class="ico_ctrl ico_play" title="播放/暂停"></a><a href="javascript:next_music();" id="next" class="ico_ctrl ico_next" title="下一首"></a><a href="javascript:m_mute();" id="volume" class="ico_ctrl ico_vol" title="音量"></a><a><input id="range" type="range" min="0" max="10" value="5" onchange="volume(this.value);"></a><a href="javascript:like_music();" id="like_music" class="ico_ctrl ico_like" title="赞"></a><a download href="" id="dl_music" class="ico_ctrl ico_share" title="下载" onClick="showtip();"></a><a href="javascript:showlist();" class="ico_ctrl ico_list" title="展开列表"></a><a href="javascript:showlrc();" class="ico_lyric ico_lyric_open" title="显示歌词">词</a></div></div><div class="s-toggle"><a href="javascript:toggle();" class="ico_ctrl ico_fold" title="折叠"></a></div></div><div id="lrcwarp" class="lrcwarp"><span id="lrc" class="s-lrc"></span></div><div id="listwarp" class="listwarp"><div class="upload"><div class="titlebox"><span class="ptitle">播放列表</span><span class="ani_border"></span></div><div class="addbox"><input id="add_music" type="text" placeholder="网易云id" /><a href="javascript:add_music();">+</a></div></div><div id="musiclist" class="musiclist"></div></div><audio id="player"></audio>';
 $("#hiyougaplayer").html(txt);
 var oAudio = $("#player")[0];
 oAudio.volume = 0.5;
@@ -23,6 +23,7 @@ $.getScript("list.js", function() {
         $("#player").attr("src",mp3_info.mp3);
 		$("#like_music").attr("href","javascript:like_music("+id+");");
 		$("#dl_music").attr("href",mp3_info.mp3);
+		$("#pre").attr("nowdata",id);
         album.css("background-image","url('" + mp3_info.cover + "?param=100y100')");
         music_name.html(mp3_info.music_name);
         artist.html(mp3_info.artists);
@@ -68,6 +69,13 @@ function m_play() {
         }
     }
 }
+function prev_music(id) {
+	oAudio.pause();
+	if (!oAudio.paused && lrc != "no") {
+        clearInterval(lrc_interval);
+    }
+	load_music(id);
+}
 function next_music() {
     oAudio.pause();
     if (!oAudio.paused && lrc != "no") {
@@ -83,6 +91,9 @@ function load_music(id) {
         $("#player").attr("src", mp3_info.mp3);
 		$("#like_music").attr("href", "javascript:like_music("+id+");");
 		$("#dl_music").attr("href", mp3_info.mp3);
+		var preid = $("#pre").attr("nowdata");
+		$("#pre").attr("href","javascript:prev_music("+preid+");");
+		$("#pre").attr("nowdata",id);
         album.css("background-image", "url('" + mp3_info.cover + "?param=100y100')");
         music_name.html(mp3_info.music_name);
         artist.html(mp3_info.artists);
